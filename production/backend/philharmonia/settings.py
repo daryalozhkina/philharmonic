ab24a844142fff98a6809cf3a752d5a5388139fe
ь <das.lozh19@mail.ru>
2022-02-19 11:29:52 +0500

    update

--- production/backend/philharmonia/settings.py
+++ production/backend/philharmonia/settings.py
@@ -136,11 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authapp.UserProfile'
 LOGIN_REDIRECT_URL = '/'
 
-
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-]
+CORS_ALLOW__ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:8000',
+#     'http://localhost:8000',
+# ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
